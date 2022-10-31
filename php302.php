173dfcaa498204ba23e240e36b72a25d1b9ded43
SombatBuekeaw <airsupansa23@gmail.com>
2022-10-31 12:05:48 +0700

    first commit

---
+++ php302.php
@@ -0,0 +1,14 @@
+<?php
+$message = "สวัสดีชาวเเมกซ์";
+$fname = "ลุงป้อม";
+$age = 2000;
+$son = 30;
+$image ='<img src="https://pprp.or.th/wp-content/uploads/2022/09/B63C880C-2D52-4272-BAAC-58B6E73F40AB-775x517.jpg" alt="v">'; 
+echo "<h1>$message</h1>";
+echo "<hr/>";
+echo "<h2>My name is $fname</h2>";
+echo "<p>My age is $age</p>";
+echo "<p>My son have $son คน</p>";
+echo "<br>";
+echo $image;
+?>
